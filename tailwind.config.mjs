c9c98c1d2d61bc0226f2058f91d3f48f659a70ce
Alejandro Giraldo S <giraldosernaalejandro16@gmail.com>
2024-03-27 16:59:10 -0500

    Colors added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,5 +10,27 @@ export default {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {},
+        },
+        dark: {
+          colors: {
+            primary: {
+              DEFAULT: "#ce9f44",
+              foreground: "#000000",
+            },
+            focus: "#ce9f44",
+            primaryDark: "121e41",
+            primarylight: "#ce9f44",
+            secondaryDark: "#292526",
+            secondarylight: "#838b90",
+          },
+        },
+        // ... custom themes
+      },
+    }),
+  ],
 };

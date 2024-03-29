4622a94e8e91cb79c7faf1da9b8e0506e166dce9
Alejandro Giraldo S <giraldosernaalejandro16@gmail.com>
2024-03-29 17:28:38 -0500

    base page about and Mision,vision,values added

---
+++ src/data/infoSGM.js
@@ -0,0 +1,34 @@
+export const aboutUs = {
+  history: {
+    title: "Historia",
+    content:
+      "This space gives you more room to expound on your message. Remember that writing effectively is an art. Start by using simple, everyday words people can easily understand. Be clear and direct to the point. Keep your thoughts flowing logically, and aim for brevity.Your ideas have a purpose, so choose words that accurately express them. Ensure your grammar is flawless as it impacts your credibility. Use the active voice whenever possible as it makes any narrative easier to read. ",
+    imgUrl:
+      "https://img.freepik.com/foto-gratis/hombre-haciendo-su-proximo-movimiento_53876-64862.jpg?t=st=1711743688~exp=1711747288~hmac=516360cc1a1eb2afc8c98d5e9212521e67f63087dd9b1fbc8f55f72ff149491b&w=1380",
+    imgDescription: "Imagen Misión",
+  },
+  mision: {
+    title: "Misión",
+    content:
+      "This space gives you more room to expound on your message. Remember that writing effectively is an art. Start by using simple, everyday words people can easily understand. Be clear and direct to the point. Keep your thoughts flowing logically, and aim for brevity.Your ideas have a purpose, so choose words that accurately express them. Ensure your grammar is flawless as it impacts your credibility. Use the active voice whenever possible as it makes any narrative easier to read. ",
+    imgUrl:
+      "https://img.freepik.com/foto-gratis/vista-aerea-equipo-empresarial_53876-124515.jpg?t=st=1711750685~exp=1711754285~hmac=385a2f00015e26b964cb4ba66101cfd3bd68f138a96bab881ae14433df913776&w=1380",
+    imgDescription: "Imagen Misión",
+  },
+  vision: {
+    title: "Visión",
+    content:
+      "This space gives you more room to expound on your message. Remember that writing effectively is an art. Start by using simple, everyday words people can easily understand. Be clear and direct to the point. Keep your thoughts flowing logically, and aim for brevity.Your ideas have a purpose, so choose words that accurately express them. Ensure your grammar is flawless as it impacts your credibility. Use the active voice whenever possible as it makes any narrative easier to read. ",
+    imgUrl:
+      "https://img.freepik.com/foto-gratis/hombre-haciendo-su-proximo-movimiento_53876-64862.jpg?t=st=1711743688~exp=1711747288~hmac=516360cc1a1eb2afc8c98d5e9212521e67f63087dd9b1fbc8f55f72ff149491b&w=1380",
+    imgDescription: "Imagen Visión",
+  },
+  values: {
+    title: "Valores",
+    content:
+      "This space gives you more room to expound on your message. Remember that writing effectively is an art. Start by using simple, everyday words people can easily understand. Be clear and direct to the point. Keep your thoughts flowing logically, and aim for brevity.Your ideas have a purpose, so choose words that accurately express them. Ensure your grammar is flawless as it impacts your credibility. Use the active voice whenever possible as it makes any narrative easier to read. ",
+    imgUrl:
+      "https://img.freepik.com/foto-gratis/vista-aerea-equipo-empresarial_53876-124515.jpg?t=st=1711750685~exp=1711754285~hmac=385a2f00015e26b964cb4ba66101cfd3bd68f138a96bab881ae14433df913776&w=1380",
+    imgDescription: "Imagen Valores",
+  },
+};

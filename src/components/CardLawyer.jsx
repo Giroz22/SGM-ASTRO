75dd6694159827b4100eee314554ba7070ca0ea0
Alejandro Giraldo S <giraldosernaalejandro16@gmail.com>
2024-04-18 13:21:33 -0500

    ContactTeam

--- src/components/CardLawyer.jsx
+++ src/components/CardLawyer.jsx
@@ -30,7 +30,7 @@ export default function CardLawyer({ props }) {
       <Divider />
       <CardFooter className="flex p-0 h-14">
         <Link
-          href={props.email}
+          href={`mailto:${props.email}?subject=SGM%20asesor%C3%ADa&body=Hola%2C%20me%20podr%C3%ADas%20ayudar%20con%20una%20asesor%C3%ADa`}
           className="w-1/2 h-full flex justify-center hover:text-[#00aae4]"
         >
           <i className="bi bi-envelope mr-2.5"></i>
@@ -38,7 +38,7 @@ export default function CardLawyer({ props }) {
         </Link>
         <Divider orientation="vertical" />
         <Link
-          href={props.whatsapp}
+          href={`https://api.whatsapp.com/send?phone=${props.whatsapp}&text=Hola,%20me%20podrias%20ayudar%20`}
           className="h-full w-1/2 flex justify-center hover:text-[#25D366]"
         >
           <i className="bi bi-whatsapp mr-2.5"></i>

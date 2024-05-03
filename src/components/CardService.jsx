aee5a57c133d4e96a72f6fc04d9b53ba7999a548
Alejandro Giraldo S <giraldosernaalejandro16@gmail.com>
2024-05-03 16:17:41 -0500

    The functionality of buttons were added

--- src/components/CardService.jsx
+++ src/components/CardService.jsx
@@ -19,6 +19,15 @@ export default function CardInfo({ props }) {
     onOpen();
   };
 
+  const onWhatsapp = () => {
+    location.href = `https://api.whatsapp.com/send?phone=+573202543661&text=Hola,%20me%20podrias%20asesorar%20con%20${props.title}`;
+  };
+
+  const onEmail = () => {
+    location.href =
+      location.href = `mailto:sgmconsultoressas@gmail.com?subject=SGM%20asesor%C3%ADa&body=Hola%2C%20me%20podr%C3%ADas%20asesorar%20con%20${props.title}`;
+  };
+
   return (
     <>
       <Button
@@ -49,7 +58,7 @@ export default function CardInfo({ props }) {
                   <Button
                     color="default"
                     variant="bordered"
-                    onPress={onClose}
+                    onPress={onWhatsapp}
                     className="border-primary text-primary hover:text-white hover:border-[#25D366] hover:bg-[#25D366]"
                   >
                     <i className="bi bi-whatsapp mr-2.5"></i>
@@ -58,7 +67,7 @@ export default function CardInfo({ props }) {
                   <Button
                     color="default"
                     variant="bordered"
-                    onPress={onClose}
+                    onPress={onEmail}
                     className="border-primary text-primary hover:text-white hover:border-[#00aae4] hover:bg-[#00aae4]"
                   >
                     <i className="bi bi-envelope mr-2.5"></i>
